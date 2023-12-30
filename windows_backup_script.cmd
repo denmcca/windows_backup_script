ee4714efe00a299539631e9e83d56fd3ad232cb6
denmcca <denmcca@gmail.com>
2023-12-29 21:34:00 -0700

    initial commit

---
+++ windows_backup_script.cmd
@@ -0,0 +1,58 @@
+@ECHO OFF
+
+setlocal enabledelayedexpansion
+:: This CMD script backs up and syncs the directories 
+:: found in the given config file (argument 1) to the 
+:: given directory (argument 2)
+
+TITLE Windows Backup Script
+
+ECHO Windows Backup initialized...
+
+SET inputFilePath=%~1
+ECHO Input File Path: %inputFilePath%
+
+SET dest=%~2
+ECHO Backup path: %dest%
+
+GOTO :MAIN
+
+@REM Start function
+:BackupDirs
+ECHO Started backing up directories...
+
+FOR /F "tokens=* delims=" %%d in (%inputFilePath%) DO (
+    ECHO =========================
+    ECHO %dest%
+    ECHO %%~Nd
+    CALL :BackupDir "%%d" "%dest%%%~Nd"
+)
+
+ECHO =========================
+
+ECHO Finished backing up directories.
+
+EXIT /B 0
+@REM End function
+
+@REM Start function
+:BackupDir
+ECHO Backing up %~1 to %~2 ...
+
+robocopy "%~1" "%~2" /MIR
+
+ECHO Completed backing up %~1.
+
+EXIT /B 0
+@REM End function
+
+@REM Start main
+:MAIN
+
+CALL :BackupDirs %inputFilePath% , %dest%
+
+PAUSE
+
+EXIT /B 0
+@REM End main
+
