6f7b44fed5df37085830d6e3194cc32ac4a99e53
denmcca <denmcca@gmail.com>
2024-01-01 10:33:38 -0700

    lowercased exit's branch flag

--- WindowsBackupScript.cmd
+++ WindowsBackupScript.cmd
@@ -33,46 +33,46 @@ goto :main
 
 @rem Start function
 :backupDirs
-echo Started backing up directories...
+    echo Started backing up directories...
 
-for /F "tokens=* delims=" %%d in (%inputFilePath%) DO (
+    for /F "tokens=* delims=" %%d in (%inputFilePath%) DO (
+        echo =========================
+
+        set sourceDir=%%~d
+        set "sourceDir=!sourceDir:\=/!"
+        @rem Use exclamation marks instead of percent signs for variables set in loop??
+        if !sourceDir:~-1!==/ set sourceDir=!sourceDir:~0,-1!
+        call :trim !sourceDir!
+        
+        call :getLast subDestDir "!sourceDir!"
+        if "!subDestDir:~-1!"=="%:" set subDestDir=!subDestDir:~0,-1!
+
+        echo subDestDir: !subDestDir!
+        set destDir=%dest%!subDestDir!
+        
+        echo sourceDir: !sourceDir!
+        echo destDir: !destDir!
+
+        call :backupDir "!sourceDir!" "!destDir!"
+    )
+
     echo =========================
 
-    set sourceDir=%%~d
-    set "sourceDir=!sourceDir:\=/!"
-    @rem Use exclamation marks instead of percent signs for variables set in loop??
-    if !sourceDir:~-1!==/ set sourceDir=!sourceDir:~0,-1!
-    call :trim !sourceDir!
-    
-    call :getLast subDestDir "!sourceDir!"
-    if "!subDestDir:~-1!"=="%:" set subDestDir=!subDestDir:~0,-1!
+    echo Finished backing up directories.
 
-    echo subDestDir: !subDestDir!
-    set destDir=%dest%!subDestDir!
-    
-    echo sourceDir: !sourceDir!
-    echo destDir: !destDir!
-
-    call :backupDir "!sourceDir!" "!destDir!"
-)
-
-echo =========================
-
-echo Finished backing up directories.
-
-exit /B 0
+exit /b 0
 @rem End function
 
 @rem Start function
 :backupDir
 if %1=="" (
     echo No value for source.
-    exit /B 1
+    exit /b 1
 )
 
 if %2=="" (
     echo No value for destination.
-    exit /B 1
+    exit /b 1
 )
 
 echo Backing up %1 to %2 ...
@@ -81,7 +81,7 @@ robocopy "%~1" "%~2" /MIR
 
 echo Completed backing up %1.
 
-exit /B 0
+exit /b 0
 @rem End function
 
 @rem Start main
@@ -91,16 +91,16 @@ call :backupDirs %inputFilePath% , %dest%
 
 pause
 
-exit /B 0
+exit /b 0
 @rem End main
 
 :trim
 echo Trimming: %~1
 set %~1=%~1
 echo Trim result: %~1
-exit /B 0
+exit /b 0
 
 :getLast
 echo Getting last token from: %~2
 if "%~N2"=="" (set "%~1=%~2") else (set "%~1=%~N2")
-exit /B 0
+exit /b 0
